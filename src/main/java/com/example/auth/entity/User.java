9980770450c4d2fe72b4c0a73862bb51718612cd
jaebinGit <tnwoql327@gmail.com>
2024-11-14 00:45:53 +0900

    Docs : id 추가

--- src/main/java/com/example/auth/entity/User.java
+++ src/main/java/com/example/auth/entity/User.java
@@ -1,9 +1,7 @@
 // User.java
 package com.example.auth.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
@@ -11,6 +9,9 @@ import lombok.Data;
 @Table(name = "users")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String username;
     private String password;
 }
