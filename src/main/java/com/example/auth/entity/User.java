9ba54e148de4fe7ad20ce4f3db380568b3f46ad1
jaebinGit <tnwoql327@gmail.com>
2024-11-22 14:06:57 +0900

    Docs : 컬럼명 수정

--- src/main/java/com/example/auth/entity/User.java
+++ src/main/java/com/example/auth/entity/User.java
@@ -10,6 +10,7 @@ import lombok.Data;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
 
     private String username;
