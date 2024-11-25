852a9d72418a1a9a62994fe10c61af3b36b36860
jaebinGit <tnwoql327@gmail.com>
2024-11-25 15:58:43 +0900

    Docs : 생성자 주입

--- src/main/java/com/example/auth/entity/User.java
+++ src/main/java/com/example/auth/entity/User.java
@@ -5,14 +5,12 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 @Data
 @Entity
 @Table(name = "users")
-@RequiredArgsConstructor
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
