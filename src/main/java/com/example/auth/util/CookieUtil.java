1bc4b2f31bc90e4022c0ed052a3e7b10c872ea8f
jaebinGit <tnwoql327@gmail.com>
2024-11-25 19:22:04 +0900

    Docs : logging

--- src/main/java/com/example/auth/util/CookieUtil.java
+++ src/main/java/com/example/auth/util/CookieUtil.java
@@ -16,17 +16,21 @@ public class CookieUtil {
     private static final Logger logger = LoggerFactory.getLogger(CookieUtil.class);
 
     public void addCookie(HttpServletResponse response, String name, String value, long maxAge) {
-        logger.info("Set cookie");
-        Cookie cookie = new Cookie(name, value);
-        logger.info("Set HttpOnly");
-//        cookie.setHttpOnly(true);
-//        cookie.setSecure(true);
-        logger.info("Set Path");
-        cookie.setPath("/");
-        logger.info("Set MaxAge");
-        cookie.setMaxAge(60*60*24);
-        logger.info("Set Add Cookie");
-        response.addCookie(cookie);
+        logger.info("Adding cookie: {} with value: {} and maxAge: {}", name, value, maxAge);
+
+        StringBuilder cookieBuilder = new StringBuilder();
+        cookieBuilder.append(name).append("=").append(value).append("; ");
+        cookieBuilder.append("HttpOnly; ");
+        cookieBuilder.append("Path=/; ");
+        cookieBuilder.append("Max-Age=").append(60*60*24).append("; ");
+        cookieBuilder.append("SameSite=Lax; "); // 또는 "Strict"
+        // HTTPS를 사용하지 않으므로 Secure 플래그 제거
+        // if (isProduction) {
+        //     cookieBuilder.append("Secure; ");
+        // }
+
+        response.addHeader("Set-Cookie", cookieBuilder.toString());
+        logger.info("Set-Cookie header added: {}", cookieBuilder.toString());
     }
 
     public void deleteCookie(HttpServletResponse response, String name) {
